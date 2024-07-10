647465a397de60450c6236182689f53f0cd1db84
Abimbola Olaide <olaolaide@outlook.com>
2024-07-10 22:52:02 +0100

    Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,19 @@ import {
   Register,
   Login,
   DashboardLayout,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
 } from './pages/index.js';
+const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+  document.body.classList.toggle('dark-theme', isDarkTheme);
+  return isDarkTheme;
+};
+const isDarkThemeEnabled = checkDefaultTheme();
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,12 +34,42 @@ const router = createBrowserRouter([
         element: <Register />,
       },
       {
-        path: '/dashboard',
-        element: <DashboardLayout />,
-      },
-      {
         path: '/login',
         element: <Login />,
+      },
+      {
+        path: '/dashboard',
+        element: <DashboardLayout isDarkThemeEnabled={isDarkThemeEnabled} />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+          },
+          {
+            path: 'stats',
+            element: <Stats />,
+
+            errorElement: <Error />,
+          },
+          {
+            path: 'all-jobs',
+            element: <AllJobs />,
+
+            errorElement: <Error />,
+          },
+          {
+            path: 'profile',
+            element: <Profile />,
+          },
+          {
+            path: 'admin',
+            element: <Admin />,
+          },
+          {
+            path: 'edit-job/:id',
+            element: <EditJob />,
+          },
+        ],
       },
     ],
   },
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,4 +1,58 @@
-const DashboardLayout = () => {
-  return <div>DashboardLayout</div>;
+import { createContext, useState, useContext } from 'react';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { SmallSidebar, BigSidebar, Navbar } from '../components';
+import { Outlet } from 'react-router-dom';
+
+const DashboardContext = createContext();
+
+const DashboardLayout = (isDarkThemeEnabled) => {
+  const user = {
+    name: 'John',
+    age: 30,
+    avatar:
+      'https://th.bing.com/th/id/OIP.H3Xxvz-5yotdS4Xr6SfnmwHaHa?w=184&h=184&c=7&r=0&o=5&pid=1.7',
+  };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
+  };
+  const logoutUser = async () => {
+    // Logout logic
+  };
+
+  return (
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
 };
+export const useDashboardContext = () => useContext(DashboardContext);
+
 export default DashboardLayout;
